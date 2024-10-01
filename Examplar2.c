cbfb7050d6f65a72e37201028215a00d69e9c701
mlb0066 <mlb0066@mix.wvu.edu>
2024-10-01 15:45:38 -0400

    MB_100124_1545

---
+++ Examplar2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main() {
+    printf("Please enter a string to reverse.\nNote that spaces are not valid characters in the string.\n>> ");
+    char* str = malloc(8 * sizeof(int));
+    scanf("%s", str);
+    int i = 0;
+    int j = (int) strlen(str) - 1;
+    while (i < j) {
+        char c = str[i];
+        str[i] = str[j];
+        str[j] = c;
+        i++;
+        j--;
+    }
+    printf(str);
+    free(str);
+    return 0;
+}
